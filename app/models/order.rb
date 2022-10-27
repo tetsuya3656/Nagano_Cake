e039b91b39c36eccbc3cb26be83eee6443cdae9a
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-10-27 12:09:34 +0000

    [Introduction] enum導入

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { wait: 0, check: 1, making: 2, in_preparation: 3, shipped: 4 }
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,3 @@
 class OrderDetail < ApplicationRecord
+  enum making_status: { not_making: 0, wait: 1, making: 2, completed: 3 }
 end
