34050c92cb89b840db75671e350213f4501cc3ab
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-11-05 09:00:25 +0000

    [Create] 会員一覧ベース作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
+
   namespace :admin do
     resources :items, only: [:new, :show, :index, :edit, :create, :update]
-
+    resources :customers, only: [:index, :show, :edit]
   end
   # 管理者用
   # URL /admin/sign_in ...
