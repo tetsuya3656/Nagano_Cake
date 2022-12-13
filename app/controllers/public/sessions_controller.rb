37075259199715e87ce5b69d000a3db15318326b
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-12-13 11:59:16 +0000

    [Finished]ECサイト完成

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-   before_action :reject_inactive_customer, only: [:create]
+  before_action :reject_inactive_customer, only: [:create]
+
   # before_action :configure_sign_in_params, only: [:create]
+
+
 
   def reject_inactive_customer
     @customer = Customer.find_by(email: params[:customer][:email])
     if @customer
-      if @customer.valid_password?(params[:customer][:encrypted_password]) && (@customer.is_deleted == true)
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+
         redirect_to new_customer_registration_path
       end
     end
+  end
 
   def after_sign_in_path_for(resource)
     root_path
@@ -20,7 +25,8 @@ class Public::SessionsController < Devise::SessionsController
     root_path
   end
 
-  end
+
+
   # GET /resource/sign_in
   # def new
   #   super
